6dc5f59c7ab6dd7cf73cb90c605d8dbb55f6ec63
wei.zhongxin@qq.com <wei.zhongxin@qq.com>
2022-09-02 10:58:01 +0800

    引用数据修改

--- test1/reference/refer.go
+++ test1/reference/refer.go
@@ -13,7 +13,10 @@ func Quote() {
 func ArrTest() {
 	arr := []int{1, 2, 3}
 	changeArr(arr, 777)
+	var arr2 [3]int = [3]int{1, 2, 3}
+	changeArr2(arr2, 777)
 	fmt.Println("ArrTest", arr)
+	fmt.Println("ArrTest", arr2)
 }
 
 // 在函数体内修改值类型参数, 不会影响到函数外的值
@@ -25,6 +28,23 @@ func changeQuote(num *int, targeNum int) {
 	*num = targeNum
 }
 
+// 影响函数外的值
 func changeArr(arr []int, targeNum int) {
 	arr[0] = targeNum
+}
+
+// 不影响函数外的值
+func changeArr2(arr [3]int, targeNum int) {
+	arr[0] = targeNum
+}
+
+func MapTest() {
+	mp := map[string]string{"name": "zhangsan", "age": "12"}
+	changeMap(mp, "里斯")
+	fmt.Println("map", mp)
+}
+
+// 影响函数外的值
+func changeMap(mp map[string]string, targeStr string) {
+	mp["name"] = targeStr
 }
